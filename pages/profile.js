17ff71e6b254af4420963552739b14ee9ed6dd3f
Moki <mokiong1427@gmail.com>
2021-10-27 20:26:03 +0800

    Add server side page guards

--- pages/profile.js
+++ pages/profile.js
@@ -1,7 +1,28 @@
+import { getSession } from 'next-auth/client';
+
 import UserProfile from '../components/profile/user-profile';
 
 function ProfilePage() {
-  return <UserProfile />;
+    return <UserProfile />;
+}
+
+export async function getServerSideProps(ctx) {
+    const session = await getSession({ req: ctx.req });
+
+    if (!session) {
+        return {
+            redirect: {
+                destination: '/auth',
+                permanent: false,
+            },
+        };
+    }
+
+    return {
+        props: {
+            session,
+        },
+    };
 }
 
 export default ProfilePage;

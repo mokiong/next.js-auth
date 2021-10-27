59e550f9939d4ec7fa8ae77b4ec3ab5210abfc0d
Moki <mokiong1427@gmail.com>
2021-10-27 19:00:31 +0800

    Save user on database

---
+++ pages/api/auth/signup.js
@@ -0,0 +1,31 @@
+import { hashPassword } from '../../../lib/auth';
+import { connectToDatabase } from '../../../lib/db';
+
+async function handler(req, res) {
+    const data = req.body;
+
+    const { email, password } = data;
+
+    if (
+        !email ||
+        !email.includes('@') ||
+        !password ||
+        password.trim().length < 7
+    ) {
+        return res.status(422).json({ message: 'Invalid input ' });
+    }
+
+    const client = await connectToDatabase();
+
+    const db = client.db();
+    const hashedPassword = hashPassword(password);
+
+    const result = await db.collection('users').insertOne({
+        email: email,
+        password: hashedPassword,
+    });
+
+    res.status(201).json({ message: 'Created user!' });
+}
+
+export default handler;

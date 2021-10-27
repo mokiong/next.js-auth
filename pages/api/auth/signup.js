7cd4acf4ff6d43e0b6ad425fa804b31df642b70b
Moki <mokiong1427@gmail.com>
2021-10-27 19:17:39 +0800

    Save users on database

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -2,30 +2,32 @@ import { hashPassword } from '../../../lib/auth';
 import { connectToDatabase } from '../../../lib/db';
 
 async function handler(req, res) {
-    const data = req.body;
+    if (req.method === 'POST') {
+        const data = req.body;
 
-    const { email, password } = data;
+        const { email, password } = data;
 
-    if (
-        !email ||
-        !email.includes('@') ||
-        !password ||
-        password.trim().length < 7
-    ) {
-        return res.status(422).json({ message: 'Invalid input ' });
+        if (
+            !email ||
+            !email.includes('@') ||
+            !password ||
+            password.trim().length < 7
+        ) {
+            return res.status(422).json({ message: 'Invalid input ' });
+        }
+
+        const client = await connectToDatabase();
+
+        const db = client.db();
+        const hashedPassword = await hashPassword(password);
+
+        const result = await db.collection('users').insertOne({
+            email: email,
+            password: hashedPassword,
+        });
+
+        res.status(201).json({ message: 'Created user!' });
     }
-
-    const client = await connectToDatabase();
-
-    const db = client.db();
-    const hashedPassword = hashPassword(password);
-
-    const result = await db.collection('users').insertOne({
-        email: email,
-        password: hashedPassword,
-    });
-
-    res.status(201).json({ message: 'Created user!' });
 }
 
 export default handler;
